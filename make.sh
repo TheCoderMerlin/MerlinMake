678be37969a2ec7766267308b3caf42a5e8b1dd0
David Ben-Yaakov <david@tangogolfdigital.com>
2020-02-26 18:55:46 -0600

    Added license information

--- make.sh
+++ make.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# make.sh
+# Copyright (C) 2018 Tango Golf Digital, LLC
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # This script generates the command line required to use the proper version of merlin libraries both for
 # compiling a project and for executing the project.  The command is then executed.
 
